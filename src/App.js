77abca54fbaae118c8f4c94962c26540b845def1
hobosi <hobon.sj@gmail.com>
2023-08-15 23:11:12 +0900

    리액트 입문 - useState

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 import './App.css';
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem'
-  }
-
   return (
-    <>
-      {/* 이 주석은 화면에 보이지 않는다 */}
-      /* 이 주석은 화면에 보인다 */
-
-      <Wrapper>
-        <Hello name={name} color='red' isSpecial/>
-        <Hello color='pink'/>
-      </Wrapper>
-      <div style={style}>{name}</div>
-      <div className='gray-box'></div>
-    </>
-  );
+    <Counter />
+  )
 }
 
 export default App;
