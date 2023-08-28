704284725c5e62853e5d4e1bfb43bbf33f8345fa
hobosi <hobon.sj@gmail.com>
2023-08-29 01:05:39 +0900

    리액트 입문 - 함수형에서 클래스형으로 변경

--- src/Counter.js
+++ src/Counter.js
@@ -1,35 +1,40 @@
-import React, { useReducer } from 'react';
+import React, { Component } from 'react';
 
-function reducer(state, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
-}
-
-function Counter() {
-  const [number, dispatch] = useReducer(reducer, 0);
+class Counter extends Component {
+  state = {
+    counter: 0,
+    fixed: 1
+  };
 
   // 함수형 업데이트
-  const onIncrease = () => {
-    dispatch({ type: 'INCREMENT'});
-  }
+  handleIncrease = () => {
+    this.setState(
+      {
+        counter: this.state.counter + 1
+      },
+      () => {
+        console.log(this.state.counter);
+      }
+    );
+  };
+  
+  handleDecrease = () => {
+    this.setState(state => ({
+      counter: this.state.counter - 1
+    }));
+  };
+  
+  render() {
+    return (
+      <div>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.handleIncrease}>+1</button>
+        <button onClick={this.handleDecrease}>-1</button>
+        <p>고정된 값 : {this.state.fixed}</p>
+      </div>
+    );
 
-  const onDecrease = () => {
-    dispatch({ type: 'DECREMENT'});
   }
-
-  return (
-    <div>
-      <h1>{number}</h1>
-      <button onClick={onIncrease}>+1</button>
-      <button onClick={onDecrease}>-1</button>
-    </div>
-  );
 }
 
 export default Counter;
