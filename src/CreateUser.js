19d1c033d7b5e6255405795aa6f6fb02ca477a1e
hobosi <hobon.sj@gmail.com>
2023-08-28 15:29:24 +0900

    fix: 오타 수정으로 등록시 username-Input박스가 초기화 안되는 오류 수정

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function CreateUser({userName, email, onChange, onCreate}) {
+function CreateUser({username, email, onChange, onCreate}) {
   return (
     <div>
-      <input name="username" placeholder="계정명" onChange={onChange} value={userName} />
+      <input name="username" placeholder="계정명" onChange={onChange} value={username} />
       <input name="email" placeholder="이메일" onChange={onChange} value={email} />
 
       <button onClick={onCreate}>등록</button>
